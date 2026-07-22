b809a1475ec56eb925c90dac4e63f3b885ffe7ae
AY <aakashyadav1098642@gmail.com>
2026-07-23 00:31:29 +0530

    Initial Spring Boot setup

---
+++ src/main/java/com/aakash/equi_track/EquiTrackApplication.java
@@ -0,0 +1,13 @@
+package com.aakash.equi_track;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EquiTrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EquiTrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aakash/equi_track/EquiTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aakash.equi_track;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EquiTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
